7113a75f20725082d10e0f585bf631e68e974489
NotAlxT <anhntran56@gmail.com>
2023-03-07 17:23:58 -0500

    c
    c

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div>Nav
+      <Link to=''></Link>
+    </div>
   )
 }
