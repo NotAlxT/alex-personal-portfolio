c97075cf8796f863482286459f83ead91c6695b3
NotAlxT <anhntran56@gmail.com>
2023-03-11 14:37:45 -0500

    c
    c

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './footer.css'
 
 export default function footer() {
   return (
     <div>
-        
+      <div className="footer-wrapper">
+        <div className="footer-aboutMe-container">
+          <div className="footer-aboutMe">About Me
+            <div className="footer-aboutMe-content">
+              
+            </div>
+          </div>
+        </div>
+        <div className="footer-contact-container">
+          <div className="footer-contact">
+            Contact
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
