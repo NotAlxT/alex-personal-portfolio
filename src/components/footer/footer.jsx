747dafa86099c7a72bd2b5f41357b5de5071be1d
NotAlxT <anhntran56@gmail.com>
2023-03-13 14:55:57 -0400

    commit
    commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,14 +6,14 @@ export default function footer() {
     <div>
       <div className="footer-wrapper ">
         <div className="footer-aboutMe-container row">
-          <div className="footer-aboutMe col-sm">About Me
+          <div className="footer-aboutMe">About Me
             <div className="footer-aboutMe-content">
 
             </div>
           </div>
         </div>
         <div className="footer-contact-container row">
-          <div className="footer-contact col-sm">
+          <div className="footer-contact">
             Contact
           </div>
         </div>
