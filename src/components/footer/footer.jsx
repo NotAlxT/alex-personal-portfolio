f1e4627abde406ea12c8f087a5395c7bc77dbc6b
NotAlxT <anhntran56@gmail.com>
2023-03-13 05:13:31 -0400

    c
    c

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,16 +4,16 @@ import './footer.css'
 export default function footer() {
   return (
     <div>
-      <div className="footer-wrapper">
-        <div className="footer-aboutMe-container">
-          <div className="footer-aboutMe">About Me
+      <div className="footer-wrapper ">
+        <div className="footer-aboutMe-container row">
+          <div className="footer-aboutMe col-sm">About Me
             <div className="footer-aboutMe-content">
-              
+
             </div>
           </div>
         </div>
-        <div className="footer-contact-container">
-          <div className="footer-contact">
+        <div className="footer-contact-container row">
+          <div className="footer-contact col-sm">
             Contact
           </div>
         </div>
