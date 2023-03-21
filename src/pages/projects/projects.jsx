e037681c27baf28586bba5f43b8e9bf0b961ab0d
NotAlxT <anhntran56@gmail.com>
2023-03-20 22:34:50 -0400

    c
    c

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './projects.css'
 
 export default function Projects() {
   return (
     <div>
       <div className="project-container">
-        <div className="calculator">
-          <h1><Link to='./calculator'>Calculator</Link></h1>
-          <p>Calculator Info</p>
-        </div>
-        <div className="todo-list"><h1>Todo-List</h1>
-          <p>Tdo-List Info</p></div>
-        <div className="weather-app"><h1>Weather App</h1>
-          <p>Weather App Info Info</p></div>
+        <Link className="calculator"to='./calculator'>
+          <div >
+            <h1>Calculator</h1>
+            <p>Calculator Info</p>
+          </div>
+        </Link>
+        <Link className="todo-list"to='./todo-list'>
+          <div >
+            <h1>Todo-List</h1>
+            <p>Todo-List Info</p>
+          </div>
+        </Link>
+        <Link className="weather-app"to='./weather-app'>
+          <div >
+            <h1>Weather</h1>
+            <p>Weather Info</p>
+          </div>
+        </Link>
+        <Link className="clock"to='./clock'>
+          <div >
+            <h1>Clock</h1>
+            <p>Clock Info</p>
+          </div>
+        </Link>
       </div>
     </div>
   )
