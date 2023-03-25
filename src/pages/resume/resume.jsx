7574d7d5670ca051503460c4c14f9785b5789080
NotAlxT <anhntran56@gmail.com>
2023-03-24 22:34:54 -0400

    c
    c

--- src/pages/resume/resume.jsx
+++ src/pages/resume/resume.jsx
@@ -4,9 +4,24 @@ import './resume.css'
 export default function Resume() {
   return (
     <div className='resume-main-container'>
-        <div className="resume-container">
-            resume
+      <div className="resume-container">
+        <div className="resume-heading">
+          <div className="resume-name">
+            Anh (Alex) Tran
+          </div>
+          <div className="resume-title">
+            Full-Stack Developer
+          </div>
+          <div className="resume-contact">
+            AnhNTran56@gmail.com | (857) 271-8817 | Boston, MA | LinkedIn | Guthub
+          </div>
         </div>
+        <div className="resume-body">
+          <div className="resume-education"> Education
+
+          </div> 
+        </div>
+      </div>
     </div>
   )
 }
