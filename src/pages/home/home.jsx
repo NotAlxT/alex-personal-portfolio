a018e9a797070c68a5f23801e0c37211fd04595f
NotAlxT <anhntran56@gmail.com>
2023-03-30 13:23:04 -0400

    c
    c

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="home-main-container">
+        <div className="home-container">
+          <div className="home-hero" >
+            </div>
+            <div className="home-box-1"></div>
+            <div className="home-box-2"></div>
+        </div>
+      </div>
+    </div>
   )
 }
